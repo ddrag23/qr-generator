9d857d11389afe3e4533caf5b875cf5021c5740d
ddrag23 <kurodyami@gmail.com>
2021-08-27 11:05:34 +0700

    add env

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const express = require('express')
-const port = 3001
 const app = express()
 const route = require('./src/routes')
 const path = require('path')
+const PORT = process.env.PORT || 3001
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('view engine', 'ejs')
 app.set('views', './src/views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(route)
-app.listen(port, () => 'Server is listening on port :' + port)
+app.listen(PORT, () => `Server is listening on port : ${PORT}`)
